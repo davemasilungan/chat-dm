43a663c5b87cfd5d4d51311912e52576809f11a6
dhipflp <MASILUNGANDA@innovasi.com>
2023-02-27 12:39:18 -0800

    Authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,31 @@
 import Sidebar from "@/components/Sidebar";
+import SessionProvider from "@/components/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]";
+
 import "../styles/globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
       <head />
       <body>
-        <div className="flex">
-          <Sidebar />
-          {/* ClientProvider - Notification */}
-          <div className="bg-[#343541] flex-1">{children}</div>
-        </div>
+        <SessionProvider session={session}>
+          {!session ? (
+            <div>test</div>
+            <Login />
+          ) : (
+            <div className="flex">
+              <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+                <Sidebar />
+              </div>
+              {/* ClientProvider - Notification */}
+              <div className="bg-[#343541] flex-1">{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
