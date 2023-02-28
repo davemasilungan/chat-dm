898ffdfb923bdc1656c4541642ae5693e9ab8302
dhipflp <MASILUNGANDA@innovasi.com>
2023-02-27 20:30:34 -0800

    login updates

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -4,7 +4,7 @@ const NewChat = () => {
   return (
     <div className="border border-gray-700 chatRow">
       <PlusIcon className="h-4 w-4" />
-      <p>New Chat</p>
+      <p className="mr-8">New Chat</p>
     </div>
   );
 };
