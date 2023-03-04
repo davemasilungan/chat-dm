96368c7ef702a28df2efa01697a1c84e4d4f06cc
dhipflip <daybid@gmail.com>
2023-03-04 12:40:40 -0800

    update head meta

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
         <title>ChatDM</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description"  content="Chat GPT clone using OpenAI Api, Nextjs 13, Typescript, Tailwind CSS, and Firebase" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
         <link rel="icon" href="/favicon.ico"/>
         </>
     )
